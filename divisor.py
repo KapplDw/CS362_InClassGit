5c2ce5157389195970f5a3ade31f1887f9af2375
Kappldw <kappldw@oregonstate.edu>
2021-04-16 13:43:14 -0700

    Divisor file added

--- divisor.py
+++ divisor.py
@@ -1,1 +1,14 @@
 # Base File
+
+# Ask for value start at 1 to value
+# Mod value by i if 0 is divisor
+
+
+value = int(input("Enter the value you would like to find Divisors for: "))
+
+temp =[]
+for i in range(1, value + 1):
+    if (value % i == 0):
+        temp.append(i)
+
+print("Divisors are: " + str(temp) )
